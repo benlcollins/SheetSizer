c7c6bb0b0a1c06e5d12af66d5a49157cf17424d8
benlcollins <ben@benlcollins.com>
2021-03-02 16:51:03 -0500

    add day 3 code

---
+++ Day3/Code.js
@@ -0,0 +1,44 @@
+/**
+ * Add custom menu to sheet
+ */
+function onOpen() {
+  SpreadsheetApp.getUi()
+      .createMenu('Sheet Sizer')
+      .addItem('Open Sheet Sizer', 'showSidebar')
+      .addToUi();
+}
+
+/**
+ * function to show sidebar
+ */
+function showSidebar() {
+  
+  // create sidebar with HTML Service
+  const html = HtmlService.createHtmlOutputFromFile('Sidebar').setTitle('Sheet Sizer');
+  
+  // add sidebar to spreadsheet UI 
+  SpreadsheetApp.getUi().showSidebar(html);
+
+}
+
+/**
+* Get size data for a given sheet url
+*/
+function auditSheet() {
+
+  // get Sheet
+  const ss = SpreadsheetApp.getActiveSpreadsheet();
+  const sheet = ss.getActiveSheet();
+
+  // get sheet name
+  const name = sheet.getName();
+
+  // get current sheet dimensions
+  const maxRows = sheet.getMaxRows();
+  const maxCols = sheet.getMaxColumns();
+  const totalCells = maxRows * maxCols;
+
+  // output
+  SpreadsheetApp.getUi().alert(totalCells);
+
+}
